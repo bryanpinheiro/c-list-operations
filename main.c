f6d277f5131234a916f65aa5ab597e4695942090
Bryan Pinheiro <40837519+bryanpinheiro@users.noreply.github.com>
2025-02-20 14:38:48 -0300

    Implement new functions main.c

--- main.c
+++ main.c
@@ -10,17 +10,40 @@ int main()
     data d = 0;
     init_lista(&l);
 
-    inserir(&l, 0, 99); // Insere 1 na posição 0
-    inserir(&l, 0, 87); // Insere 2 na posição 0
-    inserir(&l, 0, 64); // Insere 3 na posição 0
+    inserir(&l, 0, 99); // Insere 1 na posi√ß√£o 0
+    inserir(&l, 0, 87); // Insere 2 na posi√ß√£o 0
+    inserir(&l, 0, 64); // Insere 3 na posi√ß√£o 0
 
-    if(lst_procurar(l, 2) != -1) {
-        printf("\nValor encontrado!");
+    if(lst_procurar(l, 64) != -1) {
+        printf("\nValor encontrado! \n");
     }
+
+    printf("\ninserir item seguindo a ordem.\n");
 
     lst_ins_ordenado(&l, 70);
 
     lst_imprimir(l);
+
+    printf("\ninserir item posicao final.\n");
+
+    lst_inserir_final(&l, 129);
+
+    lst_imprimir(l);
+
+    printf("\ninserir item posicao inicio.\n");
+
+    lst_inserir_inicio(&l, 10);
+
+    lst_imprimir(l);
+
+    printf("\nremover item posicao final.\n");
+
+    data r = 0;
+
+    lst_remover_final(&l, &r);
+
+    lst_imprimir(l);
+
 
     return 0;
 }
