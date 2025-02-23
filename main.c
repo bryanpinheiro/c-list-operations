d41e22b8b563ef81e5214ba35ca0cac83e20e0ca
Bryan Pinheiro <40837519+bryanpinheiro@users.noreply.github.com>
2025-02-23 12:27:36 -0300

    Update main.c

--- main.c
+++ main.c
@@ -5,45 +5,53 @@
 
 int main()
 {
-
-    lista l;
+    lista l1, l2, l3;
     data d = 0;
-    init_lista(&l);
+    init_lista(&l1, false);
+    init_lista(&l2, false);
+    init_lista(&l3, true);
 
-    inserir(&l, 0, 99); // Insere 1 na posição 0
-    inserir(&l, 0, 87); // Insere 2 na posição 0
-    inserir(&l, 0, 64); // Insere 3 na posição 0
+    inserir(&l1, 0, 99);
+    inserir(&l1, 0, 87);
+    inserir(&l1, 0, 64);
 
-    if(lst_procurar(l, 64) != -1) {
+    if(lst_procurar(l1, 64) != -1) {
         printf("\nValor encontrado! \n");
     }
 
     printf("\ninserir item seguindo a ordem.\n");
 
-    lst_ins_ordenado(&l, 70);
+    lst_ins_ordenado(&l1, 70);
 
-    lst_imprimir(l);
+    lst_imprimir(l1);
 
     printf("\ninserir item posicao final.\n");
 
-    lst_inserir_final(&l, 129);
+    lst_inserir_final(&l1, 129);
 
-    lst_imprimir(l);
+    lst_imprimir(l1);
 
     printf("\ninserir item posicao inicio.\n");
 
-    lst_inserir_inicio(&l, 10);
+    lst_inserir_inicio(&l1, 10);
 
-    lst_imprimir(l);
+    lst_imprimir(l1);
 
     printf("\nremover item posicao final.\n");
 
     data r = 0;
 
-    lst_remover_final(&l, &r);
+    lst_remover_final(&l1, &r);
 
-    lst_imprimir(l);
+    lst_imprimir(l1);
 
+    inserir(&l2, 0, 21412);
+    inserir(&l2, 0, 353);
+    inserir(&l2, 0, 64353);
+    lst_imprimir(l2);
+
+    lst_concatenar(l1, l2, &l3);
+    lst_imprimir(l3);
 
     return 0;
 }
